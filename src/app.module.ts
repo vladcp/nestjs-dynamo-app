9c5d7bae628b2c2a1c59f385e4621b219c6b7376
Tudor-Cristian Tudose <tudor.tudose31@gmail.com>
2024-04-06 14:42:09 +0300

    Dynamoose configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { DynamooseModule } from 'nestjs-dynamoose';
+import * as dynamoose from 'dynamoose';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    DynamooseModule.forRoot({
+      logger: true,
+    }),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    const ddb = new dynamoose.aws.ddb.DynamoDB({
+      credentials: {
+        accessKeyId: 'test',
+        secretAccessKey: 'test',
+      },
+      region: 'local',
+      endpoint: 'http://localhost:8001',
+    });
+    dynamoose.aws.ddb.set(ddb);
+  }
+}
